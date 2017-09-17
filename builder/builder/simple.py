5ab44788b803f20176400fd9f3945bb86ebd7d1e
Will Dicharry <wdicharry@gmail.com>
2017-09-16 19:02:23 -0600

    Implement the simple cmake build script in python.
    It's pretty dumb right now.

--- builder/builder/simple.py
+++ builder/builder/simple.py
@@ -10,11 +10,15 @@ def run_build(repository_url):
     os.chdir(checkout_directory)
     subprocess.call(["git", "submodule", "update", "--init"])
     os.chdir("..")
+
+    if os.path.exists("build-debug"):
+        os.rmdir("build-debug")
+        
     os.mkdir("build-debug")
     os.chdir("build-debug")
     subprocess.call([
         "cmake",
         "-DCMAKE_BUILD_TYPE=debug",
-        "../checkout_directory"
+        "../" + checkout_directory
     ])
     subprocess.call(["make"])
